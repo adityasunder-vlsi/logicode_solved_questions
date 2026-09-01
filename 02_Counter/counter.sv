d7819d471074f9aa34c5f690de341b1a83cf586e
Aditya sunder <as.24u10963@nitdgp.ac.in>
2026-09-01 23:03:45 +0530

    Update counter.sv

--- 02_Counter/counter.sv
+++ 02_Counter/counter.sv
@@ -1,1 +1,46 @@
 
+module counter #(
+    parameter MAX = 99,
+    parameter DATA_WIDTH = 16
+) (
+    input logic clk,
+    input logic reset,
+    input logic start,
+    input logic stop,
+    output logic [DATA_WIDTH-1:0] count
+);
+
+logic running = 0;
+
+always_ff @(posedge clk) begin
+
+    if (reset) begin
+        count <= 0;
+        running <= 0;
+    end
+
+    else if (stop) begin
+        running <= 0;
+    end
+
+    else if (start) begin
+        running <= 1;
+
+        if (count == MAX)
+            count <= 0;
+        else
+            count <= count + 1;
+    end
+
+    else if (running) begin
+
+        if (count == MAX)
+            count <= 0;
+        else
+            count <= count + 1;
+
+    end
+
+end
+
+endmodule
